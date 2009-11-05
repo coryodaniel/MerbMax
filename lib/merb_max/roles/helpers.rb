7daf67acaad90687a72138208dd6ec65314f163f
Cory ODaniel <contact@coryodaniel.com>
2009-11-05 03:08:53 -0800

    Documentation

--- lib/merb_max/roles/helpers.rb
+++ lib/merb_max/roles/helpers.rb
@@ -2,6 +2,9 @@
 #
 module Merb  
   module ResponderMixin
+
+    # The 'guts' of removing an action
+    #
     # raise NotFound as if action wasn't supported
     #
     def _remove_action_
@@ -9,12 +12,24 @@ module Merb
     end
 
     module ClassMethods
-            
+      # Removes a list of actions
+      #
+      def remove_action(*_acts)
+        before :_remove_action_, :only => _acts
+      end
+
       # Removes :edit and :new action
       def no_ui
-        before :_remove_action_, :only => [:new, :edit]
+        remove_action :new, :edit
       end
       
+      # Takes options as a normal before filter
+      #
+      # Sets actions as unsupported
+      #
+      # @example
+      #   unsupported :only => [:things, :that, :are, :unsupported]
+      #   unsupported :exclude => [:things, that, :are, :supported]
       def unsupported(opts={})
         before :_remove_action_, opts
       end
